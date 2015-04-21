5dfe463822367a6d567f151c77346d2769971d12
Romain Quilliot <romain.addweb@gmail.com>
2015-04-21 23:59:17 +0200

    Add comments

--- app_dev/vendors/framework/Routing/Router.js
+++ app_dev/vendors/framework/Routing/Router.js
@@ -1,11 +1,38 @@
 class Router {
     
+    /**
+     *
+     * Init the router
+     *
+     ***********
+     * EXAMPLE
+     ***********
+     * var Routing = new Router(new HTTP());
+     *
+     */
     constructor(HTTP) {
         this.HTTP = HTTP;
-        this.routesContainer = new RouteContainer();
     }
 
-    run() {
-        
+
+    /**
+     *
+     * Launch the router
+     * @param RoutesContainer routesContainer
+     *
+     ***********
+     * EXAMPLE
+     ***********
+     * var routesContainer = new RoutesContainer();
+     * routesContainer.add('/home', myController.myMethod);
+     *
+     * Routing.run(routesContainer);
+     *
+     */
+    run(routesContainer) {
+        var routesMatcher = new RoutesMatcher(location.pathname, routesContainer.get());
+
+        routesMatcher.check();
+        routesMatcher.listen();
     }
 }
