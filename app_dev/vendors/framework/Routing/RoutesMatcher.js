5bd5273acf51689e2ad6e43ed79ff32ab984a960
Romain Quilliot <romain.addweb@gmail.com>
2015-04-22 19:13:15 +0200

    Add routing matching

--- app_dev/vendors/framework/Routing/RoutesMatcher.js
+++ app_dev/vendors/framework/Routing/RoutesMatcher.js
@@ -1,11 +1,102 @@
 class RoutesMatcher {
 
-    construct(url, routes) {
-        this.url = url;
+    /**
+     *
+     * Init the RoutesMatcher
+     * @param string uri
+     * @param array routes
+     *
+     */
+    construct(uri, routes) {
+        this.uri = uri;
         this.routes = routes;
+        this.defaultPage = function () {};
     }
 
-    isMatching(path) {
-        
+
+    /**
+     *
+     * Format the URL to test with the current PATH
+     * @param object route
+     * @return string
+     *
+     */
+    formatRoutePath(route) {
+        var routeToParse = route.path;
+        var datas = route.datas;
+
+        for (data in datas) {
+            routeToParse = routeToParse.replace("{"+ data +"}", datas[data]);
+        }
+
+        return routeToParse;
+    }
+
+
+    /**
+     *
+     * Get datas associated with the keys
+     * @param object routeDatas
+     * @param object matchedDatas
+     * @return object
+     *
+     */
+    getDatasToInject(routeDatas, matchedDatas) {
+        var i = 1;
+        var datasToInject = {};
+
+        for (data in routeDatas) {
+            datasToInject[data] = matchedDatas[i];
+            i++;
+        }
+
+        return datasToInject;
+    }
+
+
+    /**
+     *
+     * Verify if any route matchs the current path
+     * @param string routePath
+     *
+     */
+    isMatching(routePath) {
+        return this.uri.match('^'+ routePath +'$');
+    }
+
+
+    /**
+     *
+     * Define the default page
+     * @param function callback
+     *
+     */
+    setDefaultPage(callback) {
+        this.defaultPage = callback;
+    }
+
+
+    /**
+     *
+     * Coords the routing system
+     * @return boolean
+     *
+     */
+    check() {
+        for (route in this.routes) {
+            var currentRoute = this.routes[route];
+            var routePath = this.formatRoutePath(currentRoute);
+            var matching = this.isMatching(routePath);
+
+            if (matching) {
+                var routeDatas = this.routes[route].datas;
+
+                currentRoute.callback(this.getDatasToInject(routeDatas, matching));
+                return true;
+            }
+        }
+
+        this.defaultPage();
+        return false;
     }
 }
